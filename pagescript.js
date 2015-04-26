3df9146ad338d1f5980a0cc2bcd576f9d3b1062a
Maxomann <kay.gonschior@googlemail.com>
2015-04-26 13:41:38 +0200

    mobile improvements

--- pagescript.js
+++ pagescript.js
@@ -76,7 +76,10 @@ function checkMobileControl(){
 
 ////BACKGROUND
 function resizeHeaderBackgroundImage() {
-    document.getElementById('article').style.marginTop = (window.innerHeight).toString() + "px";
+    if(window.innerWidth>835)
+        document.getElementById('article').style.marginTop = (window.innerHeight).toString() + "px";
+    else
+        document.getElementById('article').style.marginTop = 0;
 }
 
 ////PROTECTION
@@ -88,7 +91,8 @@ function placeProtected(){
     }
     
     var impressums = document.getElementsByClassName('impressum_info');
-    for(n in impressums){           impressums[n].innerHTML=he.decode("&#x3C;&#x70;&#x3E;&#x4B;&#x61;&#x79;&#x20;&#x47;&#x6F;&#x6E;&#x73;&#x63;&#x68;&#x69;&#x6F;&#x72;&#x3C;&#x2F;&#x70;&#x3E;&#xA;&#x3C;&#x70;&#x3E;&#x53;&#x63;&#x68;&#x65;&#x69;&#x64;&#x65;&#x77;&#x65;&#x67;&#x2C;&#x20;&#x32;&#x36;&#x3C;&#x2F;&#x70;&#x3E;&#xA;&#x3C;&#x70;&#x3E;&#x32;&#x30;&#x32;&#x35;&#x33;&#x20;&#x48;&#x61;&#x6D;&#x62;&#x75;&#x72;&#x67;&#x3C;&#x2F;&#x70;&#x3E;&#xA;&#x3C;&#x2F;&#x62;&#x72;&#x3E;&#xA;&#x3C;&#x70;&#x3E;&#x54;&#x65;&#x6C;&#x65;&#x66;&#x6F;&#x6E;&#x3A;&#x9;&#x30;&#x34;&#x30;&#x2F;&#x37;&#x38;&#x38;&#x39;&#x34;&#x35;&#x38;&#x38;&#x3C;&#x2F;&#x70;&#x3E;")
+    for(n in impressums){   
+impressums[n].innerHTML=he.decode("&#x3C;&#x70;&#x3E;&#x4B;&#x61;&#x79;&#x20;&#x47;&#x6F;&#x6E;&#x73;&#x63;&#x68;&#x69;&#x6F;&#x72;&#x3C;&#x2F;&#x70;&#x3E;&#xA;&#x3C;&#x70;&#x3E;&#x53;&#x63;&#x68;&#x65;&#x69;&#x64;&#x65;&#x77;&#x65;&#x67;&#x2C;&#x20;&#x32;&#x36;&#x3C;&#x2F;&#x70;&#x3E;&#xA;&#x3C;&#x70;&#x3E;&#x32;&#x30;&#x32;&#x35;&#x33;&#x20;&#x48;&#x61;&#x6D;&#x62;&#x75;&#x72;&#x67;&#x3C;&#x2F;&#x70;&#x3E;&#xA;&#x3C;&#x2F;&#x62;&#x72;&#x3E;&#xA;&#x3C;&#x70;&#x3E;&#x54;&#x65;&#x6C;&#x65;&#x66;&#x6F;&#x6E;&#x3A;&#x9;&#x30;&#x34;&#x30;&#x2F;&#x37;&#x38;&#x38;&#x39;&#x34;&#x35;&#x38;&#x38;&#x3C;&#x2F;&#x70;&#x3E;")
     }
 }
 
